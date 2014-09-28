cf80c0707d328a2e3a62b856b6deff670ca025ea
BBQ Chef <bbqnerd@googlemail.com>
2014-09-28 21:36:15 +0200

    add lib/constraints

---
+++ lib/constraints/template_constraint.rb
@@ -0,0 +1,20 @@
+# http://api.rubyonrails.org/classes/ActionDispatch/Routing/Mapper/Scoping.html#method-i-constraints-label-Dynamic+request+matching
+# suggests lib/constraints as folder
+# "An expected place for this code would be lib/constraints."
+
+class TemplateConstraint
+  def self.matches?(request)
+    filename = request.path_parameters[:filename].gsub(/[^a-zA-z0-9\/\-]+/, '')  # clean up the file name
+    path = Rails.root.join('app','views','templates')
+    re = "_#{filename}"+'\.html\.(erb|haml)'
+    files = Dir.entries(path).select {|x| x =~ /#{re}/ }
+    #Rails.logger.debug "*** Debugging TemplateConstraint"
+    #Rails.logger.debug "    filename: #{filename}"
+    #Rails.logger.debug "    path: #{path}"
+    #Rails.logger.debug "    regular expression: #{re}"
+    #Rails.logger.debug "    #{files.length} file(s) found: #{files}"
+    #Rails.logger.debug "    Result: #{files.length > 0}"
+
+    files.length > 0 # returns true/false
+  end
+end
