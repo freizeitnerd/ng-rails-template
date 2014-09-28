e96a34834ee7f78d8842b66b3ef706d412c5edfc
BBQ Chef <bbqnerd@googlemail.com>
2014-09-28 22:12:52 +0200

    add example.html.erb

--- app/assets/javascripts/ng-config/csrf.js.coffee
+++ app/assets/javascripts/ng-config/csrf.js.coffee
@@ -1,4 +1,5 @@
 app.config ($httpProvider) ->
   #TODO: This doesn't work when cached
-  authToken = $("meta[name=\"csrf-token\"]").attr("content")
+  #authToken = $("meta[name=\"csrf-token\"]").attr("content")
+  authToken = 12345
   $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken
--- config/initializers/constraints.rb
+++ config/initializers/constraints.rb
@@ -8,12 +8,12 @@ class TemplateConstraint
     path = Rails.root.join('app','views','templates')
     re = "_#{filename}"+'\.html\.(erb|haml)'
     files = Dir.entries(path).select {|x| x =~ /#{re}/ }
-    #Rails.logger.debug "*** Debugging TemplateConstraint"
-    #Rails.logger.debug "    filename: #{filename}"
-    #Rails.logger.debug "    path: #{path}"
-    #Rails.logger.debug "    regular expression: #{re}"
-    #Rails.logger.debug "    #{files.length} file(s) found: #{files}"
-    #Rails.logger.debug "    Result: #{files.length > 0}"
+    Rails.logger.debug "*** Debugging TemplateConstraint"
+    Rails.logger.debug "    filename: #{filename}"
+    Rails.logger.debug "    path: #{path}"
+    Rails.logger.debug "    regular expression: #{re}"
+    Rails.logger.debug "    #{files.length} file(s) found: #{files}"
+    Rails.logger.debug "    Result: #{files.length > 0}"
 
     files.length > 0 # returns true/false
   end
