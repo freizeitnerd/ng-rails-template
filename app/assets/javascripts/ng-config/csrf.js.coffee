22f911b289688bc94a3bba422b11a81e4e22efee
BBQ Chef <bbqnerd@googlemail.com>
2014-10-04 17:24:50 +0200

    setup rails ApplicationController to read/set XSRF-TOKEN Cookie. Delete csrf_meta_tags in application.html.erb.

--- app/assets/javascripts/ng-config/csrf.js.coffee
+++
@@ -1,5 +0,0 @@
-app.config ($httpProvider) ->
-  #TODO: This doesn't work when cached
-  #authToken = $("meta[name=\"csrf-token\"]").attr("content")
-  authToken = 12345
-  $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_action :set_csrf_cookie_for_ng   # after_filter is deprecated
+
+  def set_csrf_cookie_for_ng
+    # Angular provides a mechanism to counter XSRF. When performing XHR requests,
+    # the $http service reads a token from a cookie (by default, XSRF-TOKEN)
+    # and sets it as an HTTP header (X-XSRF-TOKEN).
+    # see:
+    # http://technpol.wordpress.com/2014/04/17/rails4-angularjs-csrf-and-devise/
+    # https://docs.angularjs.org/api/ng/service/$http#cross-site-request-forgery-xsrf-protection
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+
+protected
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
 end
